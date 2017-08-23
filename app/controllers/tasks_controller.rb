1bf65e778555304dc6d3bf982b97b7670ead0fbc
hot.spring <saku.dot@gmail.com>
2017-08-23 08:49:58 +0000

    resubmit2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,11 +46,12 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-end
 
-private
 
-# Strong Parameter
+  private
+
+  # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
   end
+end
